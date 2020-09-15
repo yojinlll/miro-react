e19abaf533a4e63d7f2dd893d4f649c0f772bf50
yojin <yojin1995@qq.com>
2020-09-15 15:30:36 +0800

    配置 jest 识别非 js 文件的导出内容

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,9 @@ module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleDirectories: ['node_modules', 'include'],
   moduleNameMapper: {
+    // 自定义 mock 匹配文件的映射内容，避免识别报错
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
+    "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js",
   },
   // 匹配测试文件
   testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
