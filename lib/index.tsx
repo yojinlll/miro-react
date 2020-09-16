4c0aeb416959fe0eab62f90cb567bf7640ceefea
yojin <yojin1995@qq.com>
2020-09-16 16:06:59 +0800

    更新 package，发布demo版本

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,18 +1,20 @@
-import React  from 'react'
-import ReactDom from "react-dom";
-// import Button from "./button";
-import Icon from "./icon/icon";
+export { default as Icon } from './icon/icon'
 
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e.target, (e.target as SVGUseElement).href)
-}
+// import React  from 'react'
+// import ReactDom from "react-dom";
+// // import Button from "./button";
+// import Icon from "./icon/icon";
 
-ReactDom.render(<div>
-  <Icon name="github"
-    className='icon-1 icon-2'
-    onClick={ fn }
-    onMouseEnter={ (e) => console.log('onMouse-Enter') }
-    onMouseLeave={ (e) => console.log('onMouse-Leave') }
-  />
-  <Icon name="game" />
-</div>, document.body.querySelector('#app'))
+// const fn: React.MouseEventHandler = (e) => {
+//   console.log(e.target, (e.target as SVGUseElement).href)
+// }
+
+// ReactDom.render(<div>
+//   <Icon name="github"
+//     className='icon-1 icon-2'
+//     onClick={ fn }
+//     onMouseEnter={ (e) => console.log('onMouse-Enter') }
+//     onMouseLeave={ (e) => console.log('onMouse-Leave') }
+//   />
+//   <Icon name="game" />
+// </div>, document.body.querySelector('#app'))
