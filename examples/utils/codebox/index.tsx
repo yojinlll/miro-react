0a10ffe96e8a70cdf6ab854c49cde7d00555b479
yojin <yojin1995@qq.com>
2020-11-09 21:28:55 +0800

    update codebox

--- examples/utils/codebox/index.tsx
+++ examples/utils/codebox/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {PrismLight as SyntaxHighlighter} from 'react-syntax-highlighter';
 import {tomorrow} from "react-syntax-highlighter/dist/esm/styles/prism";
 import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
 import javascript from 'react-syntax-highlighter/dist/esm/languages/prism/javascript';
+import { Icon } from "@lib/index";
 import "./index.scss"
 
 interface props {
@@ -13,12 +14,20 @@ interface props {
 const Codebox: React.FC<props> = (props) => {
   SyntaxHighlighter.registerLanguage('jsx', jsx);
   SyntaxHighlighter.registerLanguage('javascript', javascript);
+
+  const [toggle, setToggle] = useState(false)
+
   return (
     <div className={'code-box-wrap'}>
       {props.header}
-      <div className={'code-box-example'}>{props.children}</div>
+      <div className={`code-box-example ${toggle ? 'active-code' : 'unactive-code'}`}>
+        {props.children}
+        <div className="code-toggle" onClick={() => setToggle(!toggle)}>
+          {<Icon name={toggle ? 'arrow-up' : 'arrow-down'} style={{fill: '#329ff3'}} />}
+        </div>
+      </div>
       {
-        props.codeString && <div className={'code-box-content'}>
+        toggle && props.codeString && <div className={'code-box-content'}>
           <SyntaxHighlighter className={'code-box-Highlighter'} language="jsx" style={tomorrow}>
             {props.codeString}
           </SyntaxHighlighter>
